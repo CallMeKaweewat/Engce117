c12fc5a85043a3b48464393758b377744aa54ff0
iiuuuu <you@example.com>
2023-01-04 11:46:04 +0700

    send

---
+++ Lab/Lab 4/lab 4.2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void go(int ***p, int **z) {
+*p = z;
+}
+
+int main() {
+int *b = new int; *b = 10;
+int *c = new int; *c = 20;
+int **a;
+
+go(&a, &b);
+printf("%d %p %p %p\n", **a, *a, a, &a);
+
+go(&a, &c);
+printf("%d %p %p %p\n", **a, *a, a, &a);
+
+delete b,c;
+
+return 0;
+}
