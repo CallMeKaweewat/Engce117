f7e1b60b38bbb2d2ea5baf1919d483676e1d1fe7
iiuuuu <you@example.com>
2023-01-04 10:46:25 +0700

    4.1

---
+++ Lab/Lab 4/lab 4.1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void go( int **p, int *z ) {
+    *p = z;
+}
+
+int main() {
+    int *a, b = 10, c = 20;
+    go( &a, &b );
+    printf( "%d %p %p", *a, a, &a );
+    printf("--------------------------\n") ;
+    go( &a, &c );
+    printf( "%d %p %p", *a, a, &a );
+    return 0;
+}
+
